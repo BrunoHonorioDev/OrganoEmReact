4a7e620dded0980e0194f1312d8550a4d4dcc1ef
Bruno Honórioe <brunohonoriodev@outlook.comm>
2023-05-03 12:18:14 -0300

    Criação inicial do projeto

---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+function CampoTexto() {
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='Digite seu Nome'/>
+        </div>
+    )
+}
+
+export default CampoTexto
