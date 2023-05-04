b5e506e4764c16e6e1ca28a3e0eb9506fcf33bb2
Bruno Honórioe <brunohonoriodev@outlook.comm>
2023-05-04 12:06:44 -0300

    Criação do botão

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Banner from './componentes/Banner/Banner';
-import CampoTexto from './componentes/CampoTexto';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <CampoTexto label="Nome" placeholder="Digite seu Nome"/>
-      <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
-      <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-      </div>
+      <Formulario />
+    </div>
   );
 }
 
